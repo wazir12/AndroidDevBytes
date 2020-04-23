7245e65eb8fd7a2ccd133aba557b9a486048fefd
Lalit <wazirlalit96420@gmail.com>
2020-04-23 22:07:40 +0530

    add Video Dao and add LiveData in it for watching database change, run a query from ui and that query will run in background thread

--- app/src/main/java/com/example/dev_bytes/database/Room.kt
+++ app/src/main/java/com/example/dev_bytes/database/Room.kt
@@ -1,2 +1,21 @@
 package com.example.dev_bytes.database
 
+import androidx.lifecycle.LiveData
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+
+@Dao
+interface  VideoDao{
+//if we return only List<Database Video>, the ui thread will be blocked but
+// if we return the Live Data, the ui thread
+//will not block and the Data will be fetched in the background and
+// once new data is available the room will do update for you also
+    //LiveData will also watch for changes int he Database
+    @Query("Select * from databasevideo" )
+    fun  getVideos():LiveData<List<DatabaseVideo>>
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    fun insertAll(vararg video: DatabaseVideo)
+}
